db4e78105993d1223fb2b3b670ef003b5d4901cf
Joris327 <joris.van.bellen@gmail.com>
2025-12-18 12:43:14 +0100

    copied more advanced wip website

---
+++ docs/nav.js
@@ -0,0 +1,10 @@
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+//disclaimer: this is not my code. I took it from stack overflow.
